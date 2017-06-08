00cdb795db7b27c92ece52e36b9b9e7be14b00bc
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2017-06-08 16:06:29 +0200

    little redirect url

--- drchrono/urls.py
+++ drchrono/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 
     url(r'', include('social.apps.django_app.urls', namespace='social')),
+
+    url(r'^dummy/$', views.dummy, name='dummy'),
 ]
--- drchrono/views.py
+++ drchrono/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def dummy(request):
+    html = '<html><body>This is a little dummy page.</body></html>'
+    return HttpResponse(html)
